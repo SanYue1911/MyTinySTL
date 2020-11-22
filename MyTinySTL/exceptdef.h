24ba9da335bd1184b035f2079f65a9091883483a
SanYue丶 <sanyue1911@163.com>
2020-11-22 23:11:09 +0800

    定义了debug宏的头文件

---
+++ MyTinySTL/exceptdef.h
@@ -0,0 +1,25 @@
+#ifndef MYTINYSTL_EXCEPTDEF_H_
+#define MYTINYSTL_EXCEPTDEF_H_
+
+#include <stdexcept>
+
+#include <cassert>
+
+namespace mystl
+{
+
+#define MYSTL_DEBUG(expr) \
+  assert(expr)  
+
+#define THROW_LENGTH_ERROR_IF(expr, what) \
+  if ((expr)) throw std::length_error(what)
+
+#define THROW_OUT_OF_RANGE_IF(expr, what) \
+  if ((expr)) throw std::out_of_range(what)
+
+#define THROW_RUNTIME_ERROR_IF(expr, what) \
+  if ((expr)) throw std::runtime_error(what)
+
+}
+
+#endif // !MYTINYSTL_EXCEPTDEF_H_
